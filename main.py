874bcfbf1aa46ae6fa677619ecf4f5d76a41cfe9
Lenard Luke Llanillo <lukellanillo@gmail.com>
2025-01-21 23:56:52 +0800

    chore: Update icons on navigation bar

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
-domain_verification = st.Page("data_cleanser.py", title="Domain Verification", icon=":material/broom:")
-hubspot_match = st.Page("test.py", title="Hubspot Match", icon=":material/magnet:")
+domain_verification = st.Page("data_cleanser.py", title="Domain Verification", icon=":material/edit:")
+hubspot_match = st.Page("test.py", title="Hubspot Match", icon=":material/thumb_up:")
 
 pg = st.navigation([domain_verification, hubspot_match])
 st.set_page_config(page_title="Data manager", page_icon=":material/edit:")
